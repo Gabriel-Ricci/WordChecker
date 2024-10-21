f6121b21b4579bef6e69c73ba47bc765eb304c95
Gabriel-Ricci <gabrielr244@nycstudents.net>
2024-10-21 13:32:59 +0000

    trhr

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main{
     public static void main(String[] args)
     {
+        
         WordChecker w = new WordChecker();
         ArrayList<String> words = new ArrayList<String>();
         words.add("to");
@@ -12,6 +13,15 @@ public class Main{
         System.out.println(words);
         WordChecker x = new WordChecker(words);
         System.out.println(x.isWordChain());
+        
+        ArrayList<String> skibidi = new ArrayList<String>();
+        skibidi.add("an");
+        skibidi.add("band");
+        skibidi.add("band");
+        skibidi.add("abandon");
+        System.out.println(skibidi);
+        WordChecker j = new WordChecker(skibidi);
+        System.out.println(j.isWordChain());
         
     }
 }
